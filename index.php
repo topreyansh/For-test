f091b5780b9a98780e69b1246a1838d9a3ee9bca
preyavit <topreyanshkumar@gmail.com>
2021-08-06 11:54:52 +0530

    idenx.php file modify.

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php // Silence is golden
+
+define( 'AUTH_KEY',         'L2K,L$fY2+EyOB6a#%&?V:SA)VDjCczk+UJV8)a@0vzwn?Dfd.wLo?r`W<nx9kj4' );
+define( 'SECURE_AUTH_KEY',  '&`7JuXwBevXLne~?!O!1e];G3#g|5,AxBBZ()QI+t!#erWLrU3.nXj8sb2[&~a1e' );
+define( 'LOGGED_IN_KEY',    'a?]AUkx:+=Yk0^KU?X%:~9Cm%r)$fW@K$osRygsInSZ,3+:@:,5yn:RZg8#okeM#' );
+define( 'NONCE_KEY',        '*^&5!45ArVCaFayS0Q!> &ruZ;vrE#f+@{D?=}x0~&1j-*mrR5G?&(wp@~s}:i^%' );
+define( 'AUTH_SALT',        '&c26@xp^BQsXJGDN|^U/0Wzi@CNCW62z_#HrsV6%|x/Hc9vRdBBBd/}UNr,1(FOa' );
+define( 'SECURE_AUTH_SALT', '&NW=E>xH1Y5|8%6K9FX?<:GEv7jg@o(rvUwXPl=_|l&#8+{3Cm^xi^*J:QNdS(bd' );
+define( 'LOGGED_IN_SALT',   '8ec[L3N_<kWm},-RG~ejYc2oq?_0A$c%YfEv!1d:q$a^3iHl,eT}8vx3E_?ak6?:' );
+define( 'NONCE_SALT',       'FI/.aSr[vcQ6W^xEPa&%}Y#{;sRskeqa8TSxJ4lz:MS,}Lr_XOO}sh[J%CpDIROE' );
